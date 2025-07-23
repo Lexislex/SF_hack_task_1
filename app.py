38ee0b30d9d9daa225cbfef20355a86dec075e3a
Lex at RuVDS <test@test.com>
2025-07-23 14:18:50 +0000

    fix domain issues

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-app.config['APPLICATION_ROOT'] = '/hackathon'
+# app.config['APPLICATION_ROOT'] = '/hackathon'
 
 # Доступные типы полей для добавления
 FIELD_TYPES = {
