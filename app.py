eb5f046dfa951963da5b2801a7b87ac638df3d84
Lex at RuVDS <test@test.com>
2025-07-23 11:50:49 +0000

    fix ajax query

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = '/hackathon'
 
 # Доступные типы полей для добавления
 FIELD_TYPES = {
